ee6881ae245a6f53ef21e52377d494fb440779cb
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-31 10:51:43 -0500

    Section 1.2

---
+++ Chapter1/ride.cpp
@@ -0,0 +1,54 @@
+/*
+ID: sargish1
+LANG: C++
+PROG: ride
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int	convert(string name)
+{
+	int	i;
+	int	prod;
+
+	i = 0;
+	prod = 1;
+	while (name[i])
+	{
+		prod *= name[i] - 64;
+		i++;
+	}
+	return (prod);
+}
+
+int	main()
+{
+	ifstream	inFile("ride.in", ios::in);
+	ofstream	outFile("ride.out", ios::out);
+	string		cometName;
+	string		groupName;
+	int			cometNum;
+	int			groupNum;
+
+	if (!inFile || !outFile)
+	{
+		cerr << "Error: Could not open the input file!" << endl;
+		exit(0);
+	}
+	inFile >> cometName;
+	inFile >> groupName;
+
+	cometNum = convert(cometName) % 47;
+	groupNum = convert(groupName) % 47;
+	
+	if (cometNum == groupNum)
+		outFile << "GO" << endl;
+	else
+		outFile << "STAY" << endl;
+	return (0);
+}
+
